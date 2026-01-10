00ca4134677b2df31e56fd6f144ed1c05e7a70f1
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-01-10 20:30:57 +0530

    learned lernary operator

---
+++ chapter11(adv.python).py/3_ternary_operators.py
@@ -0,0 +1,14 @@
+
+# using ternary operator
+n = 4
+result = ('even' if n % 2 == 0 else 'odd')
+print(result)
+
+
+# normal if-else ladder
+n = 4
+if n % 2 == 0:
+    result = 'even'
+else:
+    result = 'odd'
+print(result)
