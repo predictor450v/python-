74df33ec1bb50a1978b3de6aa0a4d7c18a566446
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-01-13 21:25:31 +0530

    venv

---
+++ chapter12(enviroment)/ven.py
@@ -0,0 +1,18 @@
+'''
+python3 -m venv my_env    # Creates a virtual environment named "my_env"
+
+pip install requests  # Installs the "requests" library
+pip install numpy==1.20.0 # Installs a specific version
+
+pip list
+
+pip install --upgrade requests
+
+pip uninstall requests
+
+pip freeze > requirements.txt  # Creates the requirements file
+pip install -r requirements.txt  # Installs packages from the file
+
+deactivate
+
+'''
